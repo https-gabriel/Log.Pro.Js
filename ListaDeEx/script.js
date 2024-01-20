b9f7d369cefa86a2b544f3ea3f9c6a8118a200ee
Gabriel Farias <farias2752@gmail.com>
2024-01-19 22:36:37 -0300

    Alguns ex de javascript log.Programacao

---
+++ ListaDeEx/script.js
@@ -0,0 +1,55 @@
+// Variáveis e tipos de dados: Escreva um programa que declare duas variáveis, “nome” e “idade”, e as imprima em um console em uma frase que diga “Olá, meu nome é [nome] e eu tenho [idade] anos”.
+
+// let nome = "Gabriel"
+// let idade = 21
+
+// console.log(`Hello! My name is Gabriel, i have 21 years`)
+
+//-----------------------------------------------------------
+
+//Operadores: Crie um programa que solicite ao usuário dois números e exiba o resultado da soma, subtração, multiplicação e divisão desses números.
+
+// let numberX = Number(prompt("Informe o primeiro numero que quer exibir o resultado da soma, subtração, multiplicação e divisão"))
+// let numberY = Number(prompt("Informe o segundo numero que quer exibir o resultado da soma, subtração, multiplicação e divisão"))
+
+// let soma = numberX + numberY
+// let subtracao = numberX - numberY
+// let multiplicacao = numberX * numberY
+// let divisao = numberX / numberY
+
+// alert(`A soma de ${numberX} + ${numberY} = ${soma}`)
+// alert(`A subtração de ${numberX} - ${numberY} = ${subtracao}`)
+// alert(`A multiplicação de ${numberX} * ${numberY} = ${multiplicacao}`)
+// alert(`A divisão de ${numberX} / ${numberY} = ${divisao}`)
+
+//---------------------------------------------------------------
+
+//Condições: Escreva um programa que pergunte ao usuário sua idade e imprima se ele é maior ou menor de idade.
+
+// let idade = prompt("Informe sua idade")
+
+// if (idade < 18) {
+//     alert("Menor de idade")
+// } else {
+//     alert("Maior de idade")
+// }
+
+//---------------------------------------------------------------
+
+//Loops: Crie um loop que imprima todos os números de 1 a 100.
+
+// let number = 0
+
+// for (let index = 0; index < 11; index++) {
+//     console.log(number++)
+// }
+
+//---------------------------------------------------------------
+
+// function soma(a,b) {
+//    return  a+b;
+// }
+
+// console.log(soma(2,3))
+
+//---------------------------------------------------------------
